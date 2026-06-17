5632036ca2724ea1240be41f9a6415819c789373
Neha <nehatated07@gmail.com>
2026-06-17 14:50:18 +0530

    qr-page

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='p-10 bg-blue-300 h-screen w-full flex justify-center items-center'>
+      <div className='h-105 w-70 bg-white rounded-xl'>
+        <img
+        className='p-2 h-65 w-70 rounded-2xl' 
+        src="public/image-qr-code.png" alt="QR Code" />
+        <div className='p-2 text-center'>
+          <h2 className='text-lg font-bold'>Improve your front-end skills by building projects</h2>
+          <p className='text-gray-500 text-sm p-2'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
